f3138e8d612cfd0b6c865509bce1e1ee75b839f2
jackey <18487378910@163.com>
2020-05-12 22:42:52 +0800

    update userinfo

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/register', function (req, res) {
-  let { username, password,identity } = req.body
+  let { username, password, identity } = req.body
   if (username === 'admin') {
     //注册失败已存在
     res.send({ code: 0, msg: '注册失败,用户已存在' })
@@ -21,7 +21,7 @@ router.post('/register', function (req, res) {
           res.send({ code: 0, msg: '注册失败,用户已存在' })
         } else {
           console.log('s')
-          mysql.table('userinfo').add({ username, password,identity })
+          mysql.table('userinfo').add({ username, password, identity })
             .then(function (UserId) {
               //如果插入成功，返回插入的id
               res.cookie('user_id', UserId, { maxAge: 1000 * 60 * 24 * 24 })
@@ -45,10 +45,12 @@ router.post('/login', function (req, res) {
   res.setHeader('Cookie', ['type=ninja', 'language=javascript']);
   mysql.table('userinfo').where({ username, password }).find()
     .then(function (data) {
-      console.log(data)
-      //返回结果 Object
-      res.cookie('user_id', data.id, { maxAge: 1000 * 60 * 24 * 24 })
-      res.send({ code: 1, msg: '登录成功' })
+      if (data.id) {
+        res.cookie('user_id', data.id, { maxAge: 1000 * 60 * 24 * 24 })
+        res.send({ code: 1, msg: '登录成功', user_id: data.id, identity: data.identity, username: data.username, isbeauty: data.isbeauty })
+      }else{
+        res.send({ code: 0, msg: '登录失败，用户名或密码出错！'})
+      }
     })
     .catch(function (e) {
       console.log(e);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+let mysql = require('../db/db_test')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/userDetail', function (req, res, next) {
+  let { position, head, desc, user_id,company,birth,gender,exprience,age,salary,gratuated ,address,email,mobile,nickname} = req.body
+  let isbeauty = 1
+  mysql.table('userinfo').where({ id: user_id }).update({nickname, position, head, desc,isbeauty,company,birth,gender,exprience,age,salary,gratuated,address,email,mobile })
+    .then(success => {
+      if (success) {
+        res.send({ code: 1, msg: '信息添加成功' })
+      } else {
+        res.send({ code: 0, msg: '信息添加失败',success })
+      }
+    }).catch(err=>{
+      return new Error(err)
+    })
 });
 
 module.exports = router;
